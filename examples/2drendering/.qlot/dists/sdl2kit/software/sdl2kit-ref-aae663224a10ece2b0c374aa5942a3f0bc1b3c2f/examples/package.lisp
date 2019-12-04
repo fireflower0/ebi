0aaca3a9f77d1e24a204d4cf4f9553fc6d00dfb6
fireflower0 <fireflower0@hotmail.co.jp>
2019-12-05 00:08:21 +0900

    Fixed a .gitignore

--- examples/2drendering/.qlot/dists/sdl2kit/software/sdl2kit-ref-aae663224a10ece2b0c374aa5942a3f0bc1b3c2f/examples/package.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :defpackage+-user-1)
-
-;; KIT.SDL2.TEST
-
-(defpackage+ :kit.sdl2.test
-  (:use #:cl #:alexandria #:kit.sdl2 #:kit.gl.shader #:kit.math)
-  (:export #:test-window #:simple-window #:cube-window))
--- examples/2drendering/.qlot/dists/sdl2kit/software/sdl2kit-ref-aae663224a10ece2b0c374aa5942a3f0bc1b3c2f/examples/shaders/color-pass-through.frag
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec4 interp_color;
-
-out vec4 outputColor;
-
-void main() {
-  outputColor = sqrt(interp_color); // simple gamma correction
-}
